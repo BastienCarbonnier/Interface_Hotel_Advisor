fe694e9a6dce9623240af480c24e6fc854408ed0
CedricDurand <cedricdurand13@sfr.fr>
2019-04-18 18:48:45 +0200

    Debut de la gestion de l'affichage de l'ontologie en fonction d'un hotel

--- src/app/gerant/affichage/affichage.component.ts
+++ src/app/gerant/affichage/affichage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../user/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-affichage',
@@ -10,13 +11,22 @@ export class AffichageComponent implements OnInit {
 
   private hotels : Object[];
   private selected_hotel : string ;
+  private selected_hotel_old : string ;
 
-  constructor(private serviceHotel: UserService) { }
+  constructor(private serviceHotel: UserService, private router : Router) { }
 
   ngOnInit() {
   	this.serviceHotel.getHotel().subscribe(res =>{
         this.hotels = res;
     });
   }
+
+  ngDoCheck(){
+    // Evite Angular d'inonder de requête le server Node. Il fait la requête que si l'utilisateur choisi un autre hôtel
+    if(this.selected_hotel_old != this.selected_hotel){ 
+        this.router.navigate(['/gerant',this.selected_hotel]);
+        this.selected_hotel_old = this.selected_hotel;
+      }     
+    }
 
 }
